0dd034add28706794dfb20224398f91b292153b4
cyschneck <22159116+cyschneck@users.noreply.github.com>
2019-03-20 01:47:33 -0600

    neo api returns data for description

--- pre_processing.py
+++ pre_processing.py
@@ -12,6 +12,13 @@ import requests
 def neoApiByDate(api_key):
 	# api call: Retieve a paginated list of Near Earth Objects
 	# Parameters: Page (int) and Query Size (int)
+	''' Returns: Date, is_sentry_object, links, nasa_jpl_url, absolute_magnitude_h, 
+		estimated_diameter (max, min): feet, miles, meters, kilometers,
+		close_approach_data (list): miss distance (astronmical, miles, lunar, kilometers),
+		orbiting_body (i.e. Earth, Venus, etc...),
+		epoch_date_close_approach, close_approach_date, relative_velocity (km/s, mph, km/hr),
+		neo-reference_id, is_potentially_hazardous_asteriod, id, name
+	'''
 	http_link_by_date = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-07&api_key={0}".format(api_key)
 	api_data = requests.get(http_link_by_date)
 	print("status code = {0}".format(api_data.status_code))
@@ -21,7 +28,6 @@ def neoApiByDate(api_key):
 		print("not found")
 
 	if api_data.status_code == 200:
-		print("found")
 		print("api data encoding: {0}".format(api_data.encoding))
 		api_neo_lst = api_data.json()["near_earth_objects"]
 		for date, neo_data_lst in api_neo_lst.iteritems():
@@ -32,12 +38,38 @@ def neoApiByDate(api_key):
 					print(k)
 					print("\t{0}".format(v))
 					print("\n")
+				neoApiByID(api_key, neo_data_dict['neo_reference_id'])
 			print("\n")
 
 def neoApiByID(api_key, asteriod_id):
-	# Lookup a specific Asteroid based on its NASA JPL small body (SPK-ID) ID
+	# api call: Lookup a specific Asteroid based on its NASA JPL small body (SPK-ID) ID
 	# Parameters: Asteroid (str)
+	'''Returns: designation, nasa_jpl_url, links, neo_reference_id, is_potentially_hazardous_asteriod,
+	is_sentry_object, id, name,
+	orbital_data: last_observation_date, equinox, first_observation_date, orbit_uncertainty, alphelion_distance,
+		data_arc_in_days, orbit_class (orbit_class_type, orbit_class_description, orbit_class_range),
+		mean_anomaly, orbital_period, ascending_node_longitude, orbit_id, inclination, observations_used,
+		epoch_osculation, mean_motion, jupiter_tisserand_invariant, orbit_determination_date, perihelion_time,
+		eccentricity, perihelion_argument, minimum_orbit_intersection, semi_major_axis, perihelion_distance
+	estimated_diameter (max, min): feet, miles, meters, kilometers,
+	close_approach_data (list): miss distance (astronmical, miles, lunar, kilometers),
+	'''
 	http_link_by_id = "https://api.nasa.gov/neo/rest/v1/neo/{0}?api_key={1}".format(asteriod_id, api_key)
+	api_data = requests.get(http_link_by_id)
+	print("status code = {0}".format(api_data.status_code))
+
+	if api_data.status_code != 200:
+		# 401 (Unauthorized), 402 (Forbidden), 404 (Not Found)
+		print("not found")
+
+	if api_data.status_code == 200:
+		print("api data encoding: {0}".format(api_data.encoding))
+		print("NEO BY ID")
+		neo_id_dict = api_data.json()
+		for k, v in neo_id_dict.iteritems():
+			print(k)
+			print(v)
+			print("\n")
 
 def saveNEOData(api_data):
 	# save data from api call to csv"
